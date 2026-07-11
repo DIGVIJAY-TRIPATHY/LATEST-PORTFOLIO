ca22395732606ac5d3429f0f7eea596f2701a75a
DIGVIJAY TRIPATHY <tripathydigvijay7377@gmail.com>
2026-07-11 22:57:08 +0530

    feat: add Skills component and related files for enhanced portfolio features

--- portfolio/src/App.jsx
+++ portfolio/src/App.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import Hero from './components/Hero'
 import Navbar from './components/Navbar/Navbar'
+import Skills from './components/Skills/Skills'
 
 function App() {
   return (
     <div>
       <Navbar />
       <Hero />
+      <Skills />
     </div>
   )
 }
